e22e67f74b2f04a8120bf244b2b67a0a6a81a24d
Vasya Motrya <motravasa5@gmail.com>
2025-01-31 13:44:52 +0700

    none

---
+++ ConsoleApp1/ConsoleApp1/IVirus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    interface IVirus
+    {
+        string Code { get; }
+        bool Reinfection { get; }
+        float Infection { get; }
+        float Lethality { get; }
+        void Infect(Person person);
+        bool Death(Person person);
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Person
+    {
+        private int _id;
+        private string _gender;
+        private int Age { get; set; }
+        private float _immunity;
+        private float _initialImmunity;
+        private bool _totalImmunity;
+        private const float _coefLostImmunity = 0.000017f;
+
+
+        public string Gender => _gender; 
+        public float Immunity => _immunity; 
+        public bool Status { get; set; }
+        private bool TotalImmunity => _totalImmunity;
+        public Person(int Id, string Gender, int Age, float Immunity)
+        {
+             _id = Id;
+            _gender = Gender;
+            this.Age = Age;
+            _initialImmunity = Immunity;
+
+            Status = false;
+            UpdateImmunity();
+        }
+        private void UpdateAge()
+        {
+            Age++;
+            UpdateImmunity();
+        }
+
+        private void UpdateImmunity()
+        {
+            if (Age >= 29200)
+                return;
+            _immunity = _initialImmunity - _coefLostImmunity * Age;
+        }
+    }
+}
